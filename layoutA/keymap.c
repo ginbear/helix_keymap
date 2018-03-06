012bac7856a63c624cf733af4a277b5ec96ff297
tora <shimizu@pepabo.com>
2018-03-06 22:03:36 +0900

    fix

--- layoutA/keymap.c
+++ layoutA/keymap.c
@@ -79,7 +79,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                      KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC, \
       KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                      KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
       LOWER,   KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_LBRC, KC_RBRC, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT , \
-      ADJUST,  KC_ESC,  KC_LALT, KC_LGUI, KC_ESC,  LOWER,   KC_SPC,  KC_ENT,  RAISE,   KC_RGUI, KC_LEFT, KC_UNDS, KC_EQL,  KC_BSLS \
+      ADJUST,  KC_ESC,  KC_LALT, KC_LGUI, KC_LGUI, KC_ESC,  KC_SPC,  KC_ENT,  RAISE,   KC_RGUI, KC_LEFT, KC_UNDS, KC_EQL,  KC_BSLS \
       ),
 
   /* Colemak
